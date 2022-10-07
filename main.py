a4c182cb723cd68be2b3afcc49f59ee50daa4c61
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-10-07 14:55:39 +0800

    now working on game function

--- main.py
+++ main.py
@@ -15,5 +15,28 @@ for suit in card_value.suits:
         # Adding card to the deck
         deck.append(Card(card_value.suits_values[suit], card, card_value.cards_values[card]))
 
+# (Lines below) test if playing cards are properly created in the deck
 # print(deck)
 # print(len(deck))
+
+def blackjack_game(deck):
+    '''Function for a single game of blackjack'''
+    global cards_values
+
+    # Cards for both dealer and player
+    player_cards = []
+    dealer_cards = []
+
+    # Scores for both dealer and player
+    player_score = 0
+    dealer_score = 0
+
+    # Initial dealing for player and dealer
+    while len(player_cards) < 2:
+
+        # Randomly dealing a card
+        # (Line below) choice method of the random library returns a random element from the deck list
+        player_card = random.choice(deck)
+        player_cards.append(player_card)
+        # (Line below) .remove() method removes the first item of the value of the argument
+        deck.remove(player_card)
