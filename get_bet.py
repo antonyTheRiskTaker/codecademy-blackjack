e038998b6a83a01b226dbdfd91918fdc1ae6d1d5
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-11-03 11:04:34 +0800

    build function to set bet for each round

---
+++ get_bet.py
@@ -0,0 +1,21 @@
+ # import sys #! Remove this import when testing the whole game.
+
+def getBet(maxBet):
+    '''Ask the player how much they want to bet for this round.'''
+    while True: # Keep asking until they enter a valid amount.
+        print(f'How much do you bet? (1-{maxBet}, or QUIT)')
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+        
+        if not bet.isdecimal():
+            continue # If the player didn't enter a number, ask again.
+
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            return bet # Player entered a valid bet.
+
+if __name__ == '__main__':
+    bet = getBet(5000)
+    print('Betting', bet)
