0a6abfb814fb30516a8c0ce6d78977c13b3d4aa7
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-11-06 16:06:19 +0800

    built function that shows player/dealer's hands

---
+++ display_hands.py
@@ -0,0 +1,18 @@
+from get_hand_value import getHandValue
+from display_cards import displayCards
+
+def displayHands(playerHand, dealerHand, showDealerHand):
+    '''Show the player's and dealer's cards. Hide the dealer's first card if showDealerHand is False.'''
+    print()
+    if showDealerHand:
+        print('DEALER:', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('DEALER: ???')
+        # Hide the dealer's first card:
+        #* (Line below) list concatenation applied
+        displayCards([BACKSIDE] + dealerHand[1:])
+    
+    # Show the player's cards:
+    print('PLAYER:', getHandValue(playerHand))
+    displayCards(playerHand)
