12bc3159e3dac525d45aa9da35ad50d6cc76de7a
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-11-09 10:48:40 +0800

    removed commented-out code

--- get_deck.py
+++ get_deck.py
@@ -2,12 +2,6 @@ import random
 
 import constants
 
-# Set up the suit constants
-# HEARTS = chr(9829)
-# DIAMONDS = chr(9830)
-# SPADES = chr(9824)
-# CLUBS = chr(9827)
-
 #! Later we will try to rewrite the code applying OOP
 # class Card:
 
